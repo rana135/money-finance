8804341f7042116e6797e7111dc0f66d50887ff4
rana135 <rana286090@gmail.com>
2022-02-17 03:48:50 +0600

    third part almost done

--- js/main.js
+++ js/main.js
@@ -1,9 +1,28 @@
-    const inputFood = document.getElementById('input-food').value;
-    const inputRent = document.getElementById('input-rent').value;
-    const inputClothes = document.getElementById('input-clothes').value;
+const inputIncome = document.getElementById('input-income');
+const inputFood = document.getElementById('input-food');
+const inputRent = document.getElementById('input-rent');
+const inputClothes = document.getElementById('input-clothes');
+const totalExpenses = document.getElementById("total-expenses");
+const balance = document.getElementById("balance");
+const saveMoney = document.getElementById("save-money");
+const percentInput = document.getElementById("percent-input");
+const remBalance = document.getElementById("rem-balance");
 
-    const inputText = inputFood + inputRent + inputClothes;
-    const inputvalue = parseFloat(inputText);
+const calculateButton = document.getElementById("calculate-button").addEventListener("click", function(){
+    const foodExpenses = parseInt(inputFood.value);
+    const rentExpenses = parseInt(inputRent.value);
+    const clothesExpenses = parseInt(inputClothes.value);
+    const total = foodExpenses + rentExpenses + clothesExpenses;
+    totalExpenses.innerText = total;
+    const inputValue = parseInt(inputIncome.value);
+    // balance part start:-
+    const newBalance =  inputValue - total;
+    balance.innerText = newBalance;
     
+})
 
-    
+//percent part start:-
+const save = document.getElementById("save-input").addEventListener('click', function(){
+   
+})
+
