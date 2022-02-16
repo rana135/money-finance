e72e8d10eac02bac138232ec2057057ac8e868ce
rana135 <rana286090@gmail.com>
2022-02-17 02:43:49 +0600

    third commit done

---
+++ js/main.js
@@ -0,0 +1,9 @@
+    const inputFood = document.getElementById('input-food').value;
+    const inputRent = document.getElementById('input-rent').value;
+    const inputClothes = document.getElementById('input-clothes').value;
+
+    const inputText = inputFood + inputRent + inputClothes;
+    const inputvalue = parseFloat(inputText);
+    
+
+    
