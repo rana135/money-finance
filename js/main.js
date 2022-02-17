831061c7de1a1dc4a33b606b35fc24fa53104dd5
rana135 <rana286090@gmail.com>
2022-02-17 11:26:58 +0600

    Alhamdulillah done

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// get each input & text:-
 const inputIncome = document.getElementById('input-income');
 const inputFood = document.getElementById('input-food');
 const inputRent = document.getElementById('input-rent');
@@ -10,7 +11,7 @@ const saveInput = document.getElementById("save-input")
 const percentInput = document.getElementById("percent-input");
 const remBalance = document.getElementById("rem-balance");
 
-
+// Total income & expenses part start:-
 calculateButton.addEventListener('click', calculateFunction);
 function calculateFunction() {
     const foodExpenses = parseInt(inputFood.value);
@@ -19,22 +20,21 @@ function calculateFunction() {
     const total = foodExpenses + rentExpenses + clothesExpenses;
     totalExpenses.innerText = total;
     const inputValue = parseInt(inputIncome.value);
+    // error handling:-
     if (inputValue >= total) {
         // balance part start:-
         const newBalance = inputValue - total;
         balance.innerText = newBalance;
         const finalBalance = balance.innerText;
         return finalBalance;
-    }
+    }   
+    // error handling:-
     else{
         alert('There is not enough balance in your account.')
     }
-
 }
 
-
-
-// percent part start:-
+// percent & Remaining part start:-
 saveInput.addEventListener('click', reserveMoney);
 function reserveMoney() {
     const incomeValue = parseInt(inputIncome.value);
@@ -45,34 +45,22 @@ function reserveMoney() {
         saveMoney.innerText = saveAmount;
         remBalance.innerText = calculateFunction() - saveAmount;
     }
+     // error handling:-
     else {
         alert('There is not enough balance in your account.')
     }
 }
 
 
-// saveInput.addEventListener('click', reserveMoney);
-//    function reserveMoney(){
-//        const incomeValue = parseInt(inputIncome.value);
-//     const percentText = parseInt(percentInput.value);
-//     let percentNum = (incomeValue / 100)*percentText;
-//     if(percentNum < calculateFunction()){
-//         saveMoney.innerText =percentNum;
-//         remBalance.innerText = calculateFunction() - percentNum;
-//     }
-//     else{
-//         alert('There is not enough balance in your account.')
-//     }
-// }
 
 
 
 
-//error handaling:-
-// const failedError = document.getElementById('notify-failed');
-// else{
-//     failedError.style.display = 'block';
-// }
+
+
+
+
+
 
 
 
