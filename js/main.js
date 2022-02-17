601fc19c42eaa8c761f620d81c360cf9ba4d06fd
rana135 <rana286090@gmail.com>
2022-02-17 06:34:17 +0600

    third part done

--- js/main.js
+++ js/main.js
@@ -2,13 +2,17 @@ const inputIncome = document.getElementById('input-income');
 const inputFood = document.getElementById('input-food');
 const inputRent = document.getElementById('input-rent');
 const inputClothes = document.getElementById('input-clothes');
+const calculateButton = document.getElementById("calculate-button");
 const totalExpenses = document.getElementById("total-expenses");
 const balance = document.getElementById("balance");
 const saveMoney = document.getElementById("save-money");
+const saveInput = document.getElementById("save-input")
 const percentInput = document.getElementById("percent-input");
 const remBalance = document.getElementById("rem-balance");
 
-const calculateButton = document.getElementById("calculate-button").addEventListener("click", function(){
+
+calculateButton.addEventListener('click', calculateFunction);
+function calculateFunction(){
     const foodExpenses = parseInt(inputFood.value);
     const rentExpenses = parseInt(inputRent.value);
     const clothesExpenses = parseInt(inputClothes.value);
@@ -18,11 +22,28 @@ const calculateButton = document.getElementById("calculate-button").addEventList
     // balance part start:-
     const newBalance =  inputValue - total;
     balance.innerText = newBalance;
-    
-})
+    const finalBalance = balance.innerText;
+    return finalBalance;
+}
+
+
 
 //percent part start:-
-const save = document.getElementById("save-input").addEventListener('click', function(){
+saveInput.addEventListener('click', reserveMoney);
+   function reserveMoney(){
+       const incomeValue = parseInt(inputIncome.value);
+    const percentText = parseInt(percentInput.value);
+    let percentNum = incomeValue / 100;
+    const saveAmount = percentNum * percentText;
+    if(saveAmount < calculateFunction()){
+        saveMoney.innerText =saveAmount;
+        remBalance.innerText = calculateFunction() - saveAmount;
+    }
+    else{
+        alert('There is not enough balance in your account.')
+    }
+}
+
+
    
-})
 
